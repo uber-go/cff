86b78bb6a04132f65d168f149dcfc571ffb8dcce
Jacob Greenleaf <jacobg@uber.com>
2020-01-30 23:46:59 +0000

    Add concurrent benchmark for metrics.
    We add a new benchmark for testing contention on shared state. While
    the current implementation has no contention on shared state, the new
    metrics emitter will have shared state, so adding this benchmark so we
    can compare performance.
    
    The goal of this benchmark is to detect issues where objects that are
    shared among instances of the flow contend on global locks.
    
    Test Plan:
    ```
    BenchmarkMetricsConcurrent/Metrics-8         	  114628	      9868 ns/op	    6553 B/op	      71 allocs/op
    BenchmarkMetricsConcurrent/Metrics100-8      	    7922	    189076 ns/op	  162436 B/op	    1638 allocs/op
    BenchmarkMetricsConcurrent/Metrics500-8      	    1180	    928754 ns/op	  799767 B/op	    8044 allocs/op
    BenchmarkMetricsConcurrent/Metrics1000-8     	     621	   2135604 ns/op	 1637863 B/op	   16155 allocs/op
    BenchmarkMetricsConcurrent/Metrics1000Failed-8          584	   2052610 ns/op	 1564842 B/op	   17051 allocs/op
    ```
    
    Reviewed-on: D3883933

--- internal/tests/benchmark/benchmark_test.go
+++ internal/tests/benchmark/benchmark_test.go
@@ -1,6 +1,7 @@
 package benchmark
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/uber-go/tally"
@@ -57,6 +58,48 @@ func BenchmarkMetrics(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				metricsCaseClosure.fn(logger, scope)
 			}
+		})
+	}
+}
+
+// BenchmarkMetricsConcurrent runs some benchmarks with b.N concurrent flows, for testing contention on shared state.
+func BenchmarkMetricsConcurrent(b *testing.B) {
+	logger := zap.NewNop()
+	scope := tally.NoopScope
+
+	metricsCases := []struct {
+		name string
+		fn   metricsTestFn
+	}{
+		{
+			"Metrics", Metrics,
+		},
+		{
+			"Metrics100", Metrics100,
+		},
+		{
+			"Metrics500", Metrics500,
+		},
+		{
+			"Metrics1000", Metrics1000,
+		},
+		{
+			"Metrics1000Failed", Metrics1000Failed,
+		},
+	}
+
+	for _, metricsCase := range metricsCases {
+		metricsCaseClosure := metricsCase
+		b.Run(metricsCase.name, func(b *testing.B) {
+			wg := new(sync.WaitGroup)
+			wg.Add(b.N)
+			for i := 0; i < b.N; i++ {
+				go func() {
+					metricsCaseClosure.fn(logger, scope)
+					wg.Done()
+				}()
+			}
+			wg.Wait()
 		})
 	}
 }
