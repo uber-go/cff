b0d1c29fc702d2031165d0d1bd33779a9a97d0e0
Taiwon Chung <taiwon@uber.com>
2023-09-22 10:32:37 -0400

    fix imports

--- internal/tests/panic/panic_test.go
+++ internal/tests/panic/panic_test.go
@@ -1,10 +1,10 @@
 package panic
 
 import (
-	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"go.uber.org/cff"
 )
 
--- internal/tests/predicate/predicate_test.go
+++ internal/tests/predicate/predicate_test.go
@@ -1,7 +1,6 @@
 package predicate
 
 import (
-	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
